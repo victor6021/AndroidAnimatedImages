30a52419efb1e10c08bbddccb1fe5c6d84f7516b
victor6021 <victor60216021@gmail.com>
2019-08-21 12:53:35 -0700

    adding gif and frame by frame files.

--- app/src/main/java/com/example/animatedimage/MainActivity.kt
+++ app/src/main/java/com/example/animatedimage/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.animatedimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_next.setOnClickListener {
+            animated_image.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.cat)
+            )
+        }
+
+        button_pause.setOnClickListener {
+            animated_image.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.)
+            )
+        }
     }
 }
